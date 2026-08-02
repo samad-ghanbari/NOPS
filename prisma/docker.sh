2e9bc1085ce544cee97eb1cb45b29dcc14c671d7
Samad Ghanbari <Ghanbari.Samad@gmail.com>
2026-08-02 10:07:42 +0330

    db connection by prisma

--- prisma/docker.sh
+++ prisma/docker.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-# docker run --name nops_db --rm -dv /home/.docker/volumes/postgres/18.4/nops:/var/lib/postgresql -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=xyz -p 5433:5432 postgres:18.4
+# docker run --name nops_db --rm -dv /home/.docker/volumes/postgres/18.4/nops:/var/lib/postgresql -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=xyz -p 5432:5432 postgres:18.4
 # in psql 18+ data are in  /var/lib/postgresql   in older it was in /var/lib/postgresql/data
 
-docker run --name nops_db --rm -dv /home/.docker/volumes/postgres/18.4/nops:/var/lib/postgresql -p 5433:5432 postgres:18.4
+docker run --name nops_db --rm -dv /home/.docker/volumes/postgres/18.4/nops:/var/lib/postgresql -p 5432:5432 postgres:18.4
 
