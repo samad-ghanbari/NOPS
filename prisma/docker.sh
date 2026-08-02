4a47f32d30c78f169cea997c86ad48385fbadd91
samad ghanbari <ghanbari.samad@gmail.com>
2026-08-02 10:14:17 +0330

    Delete prisma directory

--- prisma/docker.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# docker run --name nops_db --rm -dv /home/.docker/volumes/postgres/18.4/nops:/var/lib/postgresql -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=xyz -p 5432:5432 postgres:18.4
-# in psql 18+ data are in  /var/lib/postgresql   in older it was in /var/lib/postgresql/data
-
-docker run --name nops_db --rm -dv /home/.docker/volumes/postgres/18.4/nops:/var/lib/postgresql -p 5432:5432 postgres:18.4
-
