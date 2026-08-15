a53b38e376ff0633865aa4d3a159415e8b2c5e90
Samad Ghanbari <Ghanbari.Samad@gmail.com>
2026-08-15 09:11:10 +0330

    breadcrumb module is done

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,3 +1,16 @@
+import BreadCrumb from "@/components/BreadCrumb";
+
 export default async function DashboardPage() {
-  return <h1>HOME</h1>;
+  return (
+    <>
+      <BreadCrumb
+        items={[
+          { name: "گروه", href: "/group" },
+          { name: "داشبورد", href: "/dashboard" },
+          { name: "تجهیزات" },
+        ]}
+      />
+      <h1>HOME</h1>
+    </>
+  );
 }
