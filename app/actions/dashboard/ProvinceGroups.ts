658e64417008883741fe4f3b8e5ef30a69743269
Samad Ghanbari <ghanbari.samad@gmail.com>
2026-08-22 23:32:41 +0330

    province group init

--- app/actions/dashboard/ProvinceGroups.ts
+++ app/actions/dashboard/ProvinceGroups.ts
@@ -1,13 +1,44 @@
+"use server";
+
 import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
 import { z } from "zod";
+import { ERROR_CODES } from "@/lib/constants/error";
+import { Group } from "@/lib/generated/prisma/client";
 
 const schema = z.object({ provinceId: z.uuidv7() });
 
-export async function getGroupsInfo(proviceId: string) {
+async function validate(provinceId: string) {
   const session = await auth();
 
   if (!session?.user) {
-    throw new Error("Unauthorized");
+    throw new Error(ERROR_CODES.UNAUTHENTICATED);
   }
+
+  const result = schema.safeParse({ provinceId });
+
+  if (!result.success) throw new Error(ERROR_CODES.VALIDATION_ERROR);
+}
+
+export async function getGroupCount(provinceId: string) {
+  validate(provinceId);
+
+  const groupCount: number = await prisma.group.count({
+    where: { provinceId: provinceId },
+  });
+
+  return groupCount;
+}
+
+//
+
+export async function getGroups(provinceId: string) {
+  validate(provinceId);
+
+  const groups: Group[] = await prisma.group.findMany({
+    where: { provinceId: provinceId },
+    orderBy: [{ order: "asc" }, { groupName: "asc" }],
+  });
+
+  return groups;
 }
--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { LoaderCircle } from "lucide-react";
 export default function Loading() {
   return (
     <div className="flex min-h-dvh items-center justify-center">
-      <LoaderCircle className="h-10 w-10 animate-spin text-pink-700" />
+      <LoaderCircle className="h-10 w-10 animate-spin text-sky-700" />
     </div>
   );
 }
